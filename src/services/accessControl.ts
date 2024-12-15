d9b9ffed5aa608dafda797e9ea02a4a069087b7a
Kristen Garnier <kristen@oktopod.io>
2024-12-15 12:43:38 +0100

    Init exercice

--- src/services/accessControl.ts
+++ src/services/accessControl.ts
@@ -2,25 +2,6 @@ import { AccessControl } from 'accesscontrol';
 
 const ac = new AccessControl();
 
-// Définition des permissions
-ac.grant('guest')
-  .read('profile')
-  .readOwn('data');
-
-ac.grant('user')
-  .extend('guest')
-  .createOwn('data')
-  .updateOwn('data')
-  .deleteOwn('data');
-
-ac.grant('admin')
-  .extend('user')
-  .create('profile')
-  .update('profile')
-  .delete('profile')
-  .read('data')
-  .create('data')
-  .update('data')
-  .delete('data');
+// Définissez vos permissions ici
 
 export { ac };
--- src/services/hashing.ts
+++ src/services/hashing.ts
@@ -1,16 +1,7 @@
-import argon2 from 'argon2';
-import crypto from 'node:crypto'
+// Ici dénissez les fonctions qui vont vous permettre de vérifier et générer des mots de passes sécurisés
 
 export const hashPassword = async (password: string): Promise<{ password: string, salt: string }> => {
-  const salt = crypto.randomBytes(16).toString('hex');
-  return {
-    password: await argon2.hash(`${password}${salt}`, {
-      type: argon2.argon2id,
-      memoryCost: 2 ** 16,
-      timeCost: 3,
-      parallelism: 1
-    }), salt
-  };
+
 };
 
 export const verifyPassword = async (
@@ -18,9 +9,4 @@ export const verifyPassword = async (
   salt: string,
   hashedPassword: string
 ): Promise<boolean> => {
-  try {
-    return await argon2.verify(hashedPassword, `${password}${salt}`);
-  } catch (err) {
-    return false;
-  }
 };
